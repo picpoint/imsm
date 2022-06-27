c1b12b0ab5967f3c39a8e29adcd7035e9c160c95
picpoint <picpoint@yandex.ru>
2022-06-27 08:53:36 +0300

    work with slider on index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,31 +14,50 @@
 
     <section class="index">
         <div class="index__wrapper">
-            <div class="index__regblockwrp">
-                <div class="index__regblock">
+
+            <div class="index__regblock">
+                <div class="index__regblockwrp">
                     <i class="fas fa-user"></i>
                     <a href="#">Личный кабинет</a>
                 </div>
             </div>
 
-            <div class="index__logoblockwrp">
-                <div class="index__logoblock">
+            <div class="index__logoblock">
+                <div class="index__logoblockwrp">
                     <div class="index__logoblockphone">
                         <i class="fas fa-phone-alt"></i>
-                        <span>8-800-555-55-55</span>
+                        <span>+7-918-804-44-27</span>
                     </div>
                     <div class="index__logoblockpict">
                         <a href="#">
-                            <img src="#" alt="logo">
+                            <img src="/public/assets/users/img/logo.jpg" alt="logo">
                         </a>
                     </div>
                     <div class="index__logoblockbasket">
                         <a href="#">
-                            <img src="#" alt="basket">
+                            <img src="/public/assets/users/img/basket.jpg" alt="basket">
                         </a>
                     </div>
                 </div>
             </div>
+
+            <div class="index__menublock">
+                <div class="index__menublockwrp">
+                    <a href="#">Каталог</a>
+                    <a href="#">Акции</a>
+                    <a href="#">Доставка</a>
+                    <a href="#">Оплата</a>
+                    <a href="#">Контакты</a>
+                    <a href="#">О нас</a>
+                </div>
+            </div>
+
+            <div class="index__sliderblock">
+                <div class="index__sliderblockwrp">
+                    <img src="#" alt="slide">
+                </div>
+            </div>
+
         </div>
     </section>
 
