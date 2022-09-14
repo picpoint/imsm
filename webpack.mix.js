91a845adfe580409f5261013dc3a87789b8b7663
picpoint <picpoint@yandex.ru>
2022-09-14 22:55:20 +0300

    work with admin panel

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,6 +18,23 @@ const mix = require('laravel-mix');
 
 
 
+mix.styles([
+    'resources/assets/admin/plugins/fontawesome-free/css/all.min.css',
+    'resources/assets/admin/css/adminlte.min.css'
+], 'public/assets/admin/css/admin.css');
+
+
+mix.scripts([
+    'resources/assets/admin/plugins/jquery/jquery.min.js',
+    'resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js',
+    'resources/assets/admin/js/adminlte.min.js',
+    'resources/assets/admin/js/demo.js',
+], 'public/assets/admin/js/admin.js');
+
+
+// mix.copyDirectory('resources/assets/admin/plugins/fontawesome-free/webfonts', 'public/assets/admin/webfonts');
+// mix.copyDirectory('resources/assets/admin/img', 'public/assets/admin/img');
+
 
 mix.sass('resources/assets/users/scss/index.scss', '/public/assets/users/css/index.css')
     .browserSync({
